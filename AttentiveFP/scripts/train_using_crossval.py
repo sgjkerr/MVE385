1176247bb156fc5e6dc51ef2615ee8ba64fb9988
sgjkerr <38070891+sgjkerr@users.noreply.github.com>
2024-01-26 18:52:19 +0100

    Update train_using_crossval.py

--- AttentiveFP/scripts/train_using_crossval.py
+++ AttentiveFP/scripts/train_using_crossval.py
@@ -24,9 +24,7 @@ from utils import get_featurizer, GraphModel
 # parse arguments
 parser = argparse.ArgumentParser()
 
-parser.add_argument(
-    "--sheetcsv-dir", help="sheetcsv dir", default="../smalldatacsv/ornl_aisd_ex/ornl_aisd_ex_1.csv"
-)
+
 parser.add_argument("--model-name", help="graph model name", required=True)
 parser.add_argument(
     "--model-dir", help="dir to save model", default="../models"
@@ -41,7 +39,6 @@ parser.add_argument(
 )
 args = parser.parse_args()
 
-sheetcsv_dir = args.sheetcsv_dir
 
 model_name = args.model_name
 model_dir = args.model_dir.rstrip("/") + f"/{model_name}"
@@ -75,33 +72,19 @@ shortset =0
 
 
 absorb = []
-if shortset == 1:
-    for folder in sorted(os.listdir(Dataset_dir)):
-        local_dir = os.path.join(Dataset_dir, folder, "EXC-smooth.DAT")
-        df = pd.read_csv(local_dir, delim_whitespace=True)
-        row = np.squeeze(df.values[0:len(df.values),[1]])
-        absorb.append(row.transpose())
-else:
-    for i in tqdm(range(0,1044)):#tqdm(range(0,loaded_array.shape[0])):
-        absorb_row = np.array(loaded_array[i][1:-1], dtype = float)
-        absorb.append(absorb_row)
+for i in tqdm(range(0,loaded_array.shape[0])):
+    absorb_row = np.array(loaded_array[i][1:-1], dtype = float)
+    absorb.append(absorb_row)
 
 
-
-if shortset ==1:
-    for smile in tqdm(csv_data.values[0:100,[1]]): 
-
-        feat = featurizer.featurize(smile[0])
-        feature.append(feat)
-else:
-    for i in tqdm(range(0,1044)):#tqdm(range(0,loaded_array.shape[0])): 
-        encoded_smiles_str = loaded_array[i][0]
-        decoded_smiles_str = np.char.decode(encoded_smiles_str, encoding='cp037')
-        smile = str(decoded_smiles_str)
+for i in tqdm(range(0,loaded_array.shape[0])): 
+    encoded_smiles_str = loaded_array[i][0]
+    decoded_smiles_str = np.char.decode(encoded_smiles_str, encoding='cp037')
+    smile = str(decoded_smiles_str)
 
 
-        feat = featurizer.featurize(smile)[0]
-        feature.append(feat)
+    feat = featurizer.featurize(smile)[0]
+    feature.append(feat)
 ## read smiles as x and spectra as y
 
 f1 = [feature[0]]
